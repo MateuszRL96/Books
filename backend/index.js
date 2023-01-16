96c6f9f108ea1fccd311e494d8fac5c45e3a3be8
MateuszRL96 <mati.kulec12@gmail.com>
2023-01-16 16:30:34 +0100

    nowy commit

---
+++ backend/index.js
@@ -0,0 +1,83 @@
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express()
+
+const db = mysql.createConnection(
+    {
+        host:"localhost",
+        user:"root",
+        password:"1234",
+        database:"test2"
+    }
+)
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res)=>
+{
+    res.json("to jest Backend")
+})
+
+app.get("/books",(req,res)=>
+{
+    const q ="SELECT *FROM books"
+    db.query(q,(err, data)=>
+    {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req, res)=>
+{
+    const q ="INSERT INTO books (`title`,`desc`,`price`,`cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+    ];
+
+    db.query(q,[values], (err, data)=>
+    {
+        if(err) return res.json(err)
+        return res.json("Book has been created");
+    })
+});
+//Delete
+app.delete("/books/:id", (req,res)=>
+{
+    const bookId = req.params.id;
+    const q = "DELETE from books where id = ?"
+
+    db.query(q,[bookId], (err,data)=> {
+        if(err) return res.json(err)
+        return res.json("Book has been deleted");
+    })
+});
+//Update
+app.put("/books/:id", (req,res)=>
+{
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+    ]
+
+    db.query(q,[...values,bookId], (err,data)=> {
+        if(err) return res.json(err)
+        return res.json("Book has been updated");
+    });
+});
+
+app.listen(8800, ()=>
+{
+    console.log("Connected to Backend!")
+});
